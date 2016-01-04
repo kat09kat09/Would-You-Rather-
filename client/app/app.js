ef233cc22a8020bfb1b149128787755ea4c18055
kat09kat09 <kat09kat09@gmail.com>
2016-01-04 16:33:22 -0500

    Setup routing using ui-router & skeleton of controllers

---
+++ client/app/app.js
@@ -0,0 +1,29 @@
+angular.module('wyr', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/home'); 
+
+  //set up multiple views on the home page
+  $stateProvider
+    .state('home', {
+      url:'/home',
+      views: {
+        'columnOne@showQ':  {
+          templateUrl: 'showQ.html'
+          controller: showQCtrl
+        },
+        'columnTwo@submitQ': {
+          templateUrl: 'submitQ.html',
+          controller: submitQCtrl
+        }
+      }
+    })
+})
+
+.controller('showQCtrl', function ($scope){
+
+})
+
+.controller('submitQCtrl', function ($scope){
+  
+})  
