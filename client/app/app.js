552161f30b90db0b3a872a2d794e3de340268259
kat09kat09 <kat09kat09@gmail.com>
2016-01-04 23:59:24 -0500

    implemented addQ & nextQ fxns

--- client/app/app.js
+++ client/app/app.js
@@ -21,10 +21,42 @@ angular.module('wyr', ['ui.router'])
     })
 }])
 
-.controller('showQCtrl', function ($scope){
+.controller('showQCtrl', ['$scope', 'NextQ', function ($scope, NextQ){
   $scope.testA= 'this works'; 
+  $scope.question= ['option A', 'option B']; 
+  $scope.nextQ= function () {
+    $scope.question= NextQ.nextQ();
+  }  
+
+}])
+
+.controller('submitQCtrl', ['$scope', 'NextQ', function ($scope, NextQ){
+  $scope.testB= 'this works as well'; 
+  $scope.addQ= function (optionA, optionB) {
+    console.log('gets here'); 
+    NextQ.addQ(optionA, optionB); 
+  }
+}])  
+
+.factory('NextQ', function () {
+  var questions= [['option A', 'option B'], ['option C', 'option D']]; 
+  var idx=0; 
+  var nextQ= function () {
+    idx++; 
+    if(idx>= questions.length) {
+      idx=0; 
+    }
+    return questions[idx]; 
+  };
+
+  var addQ= function (optionA, optionB) {
+    questions.push([optionA, optionB]); 
+    console.log('questions', questions); 
+  };
+
+  return {
+    nextQ: nextQ,
+    addQ: addQ
+  }
 })
 
-.controller('submitQCtrl', function ($scope){
-  $scope.testB= 'this works as well'; 
-})  
