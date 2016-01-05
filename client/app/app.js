c94c95039284a407cf951e9c5061a86f4a63ad7c
kat09kat09 <kat09kat09@gmail.com>
2016-01-04 19:31:55 -0500

    set up ui-router to work w/ nested views

--- client/app/app.js
+++ client/app/app.js
@@ -1,29 +1,30 @@
 angular.module('wyr', ['ui.router'])
 
-.config(function($stateProvider, $urlRouterProvider){
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/home'); 
 
   //set up multiple views on the home page
   $stateProvider
     .state('home', {
       url:'/home',
-      views: {
-        'columnOne@showQ':  {
-          templateUrl: 'showQ.html',
-          controller: 'showQCtrl'
-        },
-        'columnTwo@submitQ': {
-          templateUrl: 'submitQ.html',
-          controller: 'submitQCtrl'
-        }
-      }
+      templateUrl: 'app/views/home.html'
     })
-})
+    .state('home.showQ', {
+      url:'/showQ',
+      templateUrl: 'app/views/home-showQ.html',
+      controller: 'showQCtrl'
+    })
+    .state('home.submitQ', {
+      url:'/submitQ',
+      templateUrl: 'app/views/home-submitQ.html',
+      controller: 'submitQCtrl'
+    })
+}])
 
 .controller('showQCtrl', function ($scope){
-
+  $scope.testA= 'this works'; 
 })
 
 .controller('submitQCtrl', function ($scope){
-
+  $scope.testB= 'this works as well'; 
 })  
