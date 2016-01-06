2e6ad5fcaf13a716298ddfc2e9c5d8446140654b
kat09kat09 <kat09kat09@gmail.com>
2016-01-05 23:06:03 -0500

    moved server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+  var express     = require('express'),
+      mongoose    = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost/questions'); // connect to mongo database named questions
+
+// configure our server with all the middleware and and routing
+require('./config/middleware.js')(app, express);
+
+// export our app for testing and flexibility, required by index.js
+
+app.listen(process.env.PORT||8000);
+
+module.exports = app;
