a71d0c16f02ae96bf709274615bc814a1a96c84c
kat09kat09 <kat09kat09@gmail.com>
2016-01-05 23:24:07 -0500

    edited path for require statement

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 mongoose.connect('mongodb://localhost/questions'); // connect to mongo database named questions
 
 // configure our server with all the middleware and and routing
-require('./config/middleware.js')(app, express);
+require('./server/config/middleware.js')(app, express);
 
 // export our app for testing and flexibility, required by index.js
 
